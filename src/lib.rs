e90d231c746b20da930eb9c67dc2f2dadc581728
Aiman Najjar <aiman@aimannajjar.com>
2026-05-30 12:27:37 -0400

    refactors

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{fmt::Display, io};
 mod object;
 mod repo;
 
@@ -13,3 +13,13 @@ pub enum GitError {
     CLIError(String),
 }
 
+
+impl Display for GitError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            GitError::IOError(e) => writeln!(f, "{}", e.to_string()),
+            Self::ObjectError(e) => writeln!(f, "{e}"),
+            Self::CLIError(e) => writeln!(f, "{e}"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,19 @@ use codecrafters_git::{GitError, cli::Git};
 fn main() {
     let mut git = match Git::from_env() {
         Ok(git) => git,
-        Err(GitError::CLIError(e)) => {
+        Err(e) => {
             eprintln!("{}", e);
-            std::process::exit(1);
-        },
-        Err(e) => panic!("unexpected error: {:?}", e),
-    };
-
-    match git.run() {
-        Err(GitError::IOError(e)) if e.kind() == std::io::ErrorKind::BrokenPipe => {}
-        Err(GitError::IOError(e)) => {
-            eprintln!("i/o error: {}", e.to_string());
             std::process::exit(1);
         }
-        Err(GitError::ObjectError(e)) => { 
-            eprintln!("{}", e);
-            std::process::exit(2);
-        },
-        Err(e) => panic!("unexpected error: {:?}", e),
-        Ok(_) => (),
     };
 
+    if let Err(e) = git.run() {
+        match e {
+            GitError::IOError(e) if e.kind() == std::io::ErrorKind::BrokenPipe => {}
+            _ => {
+                eprintln!("{}", e);
+                std::process::exit(1);
+            }
+        };
+    }
 }
