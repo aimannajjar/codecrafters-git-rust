dd53e4742416a4d598b48366a62e461aa87e378e
Aiman Najjar <aiman@aimannajjar.com>
2026-06-01 22:10:27 -0400

    write-tree sha output

--- src/cli.rs
+++ src/cli.rs
@@ -310,7 +310,9 @@ impl<O: io::Write> Git<O> {
     // recurisvely generate tree objects starting from current working directory
     // todo: limit generation to staged area
     fn write_tree(&self) -> GitResult<()> {
-        tree::Tree::write_tree(PathBuf::from("./"))?;
+        let hash = tree::Tree::write_tree(PathBuf::from("./"))?;
+        let hash = const_hex::encode(hash);
+        println!("{}", hash);
         Ok(())
     }
 }
